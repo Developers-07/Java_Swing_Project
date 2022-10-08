0ebb43397870386ee87053707326bb90b698e60b
Zishan Chowdhury <zishanc901@gmail.com>
2022-10-09 00:01:04 +0600

    Beginner Project

---
+++ BasicJava/src/basicjava/BasicJava.java
@@ -0,0 +1,14 @@
+
+package basicjava;
+
+public class BasicJava {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hi");
+        System.out.println("Zishan");
+        System.out.println("Hello World");
+        System.out.println("01752188022");
+    }
+    
+}
